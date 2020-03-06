516adde842c07a94164890ac69a803410d6b8b84
eric <eric@hellman.net>
2020-03-05 21:13:08 -0500

    bad fix

--- questionnaire/templatetags/landings.py
+++ questionnaire/templatetags/landings.py
@@ -4,7 +4,7 @@ register = django.template.Library()
 
 @register.simple_tag(takes_context=True)
 def render_with_landing(context, text):
-    if not 'landing_object' in context.has_key and 'runinfo' in context:
+    if not 'landing_object' in context and 'runinfo' in context:
         landing = context['runinfo'].landing
         context['landing_object'] = landing.content_object if landing else ''
     if text:
